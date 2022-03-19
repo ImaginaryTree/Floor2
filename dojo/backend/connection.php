9b2d7ce486f059fdd4b1706a00ae3da0bf675a37
WeebTech <45412717+ImaginaryTree@users.noreply.github.com>
2022-03-19 14:15:50 +0700

    Update connection.php

--- dojo/backend/connection.php
+++ dojo/backend/connection.php
@@ -1,5 +1,7 @@
 <?php
+    //connecting to mysql database
     $connect = mysqli_connect("localhost","root","","elearning");
+    //checking if connection to database false or true
     if (!$connect) {
       die("Connection failed: " . mysqli_connect_error());
     }
